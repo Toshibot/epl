47d0170747027e10e959035561fd85ed3e86f80c
Toshi Stewart <toshi.stewart@gmail.com>
2018-07-27 11:46:00 +0200

    Gamedays 2 and 3 added to roundCalc.js

--- app/js/_dev/_functions/_round-calc.js
+++ app/js/_dev/_functions/_round-calc.js
@@ -4,12 +4,17 @@ function roundCalc(d) {
     var date = currentDate.getDate();
     var year = currentDate.getFullYear();
 
-    // Round 1
+    // Gameday 1
     if (year == "2018" && month <= 8) {
         return 1;
 
-    // Round 2    
+    // Gameday 2    
     } else if (year == "2018" && month == 8 && date <= 20){
-        return 2
+        return 2;
+
+    // Gameday 3
+    } else if (year == "2018" && month == 8 && date <= 27){
+        return 3;
+
     }
 }
